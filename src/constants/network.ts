a290da39c8e343538ea8c5b3d8784a7c95a79956
aurora <kexin.yang@aelf.io>
2024-02-23 10:56:18 +0800

    refactor: 💡 modify example

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -10,14 +10,14 @@ export const BackEndNetWorkMap: Record<string, WebsiteNetworkConfig> = {
     dev: {
       name: 'aelf Testnet',
       networkType: 'TESTNET',
-      cmsUrl: 'https://testhome.symbolmarket.io/cms/', //'http://192.168.66.62:3104/', //'https://localtest-applesign.portkey.finance/cms/',
-      s3Url: 'https://symbolmarket-dev.s3.ap-northeast-1.amazonaws.com/', //'http://192.168.11.224:8060/assets/', //'https://portkey-cms-dev.s3.ap-northeast-1.amazonaws.com/',
+      cmsUrl: 'https://xxx/cms/',
+      s3Url: 'https://xxx-dev.s3.ap-northeast-1.amazonaws.com/',
     },
     mainnet: {
       name: 'aelf Mainnet',
       networkType: 'MAIN',
-      cmsUrl: 'https://cms.portkey.finance/',
-      s3Url: 'https://portkey-cms-mainnet.s3.ap-northeast-1.amazonaws.com/',
+      cmsUrl: 'https://xxx/',
+      s3Url: 'https://xxx.s3.ap-northeast-1.amazonaws.com/',
     },
   },
   // Add More ...
